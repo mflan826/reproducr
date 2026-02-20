29c76f19a615dccd7138f5a2ffab25eba69e7477
Eric Peterson <akapeterser@gmail.com>
2026-02-20 08:29:22 -0500

    Remove bibtex parser

--- parser.py
+++ parser.py
@@ -4,8 +4,6 @@ Parse raw data output
 
 from pathlib import Path
 
-# remember to install "bibtexparser<2" because it's more forgiving
-import bibtexparser
 from lxml import etree
 
 
@@ -72,7 +70,6 @@ def parse_efetch_page(results: str) -> list[dict]:
 
     tree = etree.fromstring(results)
 
-
     for article in tree.xpath("//*[local-name()='article']"):
 
         record = parse_efetch(article)
@@ -111,25 +108,3 @@ def parse_jmir_xml_file(xml_file_path: str | Path) -> tuple[str, list[str]]:
         data_results.append(result.text)
 
     return doi, data_results
-
-
-def parse_bibtex_file(bibtex_file_path: str | Path) -> list[tuple[str, list[str]]]:
-    """
-    Extract data from bibtex file
-
-    :param bibtex_file_path: Path to bibtex file with raw data
-    :type bibtex_file_path: str | Path
-    :return: List of all the articles and their keywords found in the citation
-    :rtype: list[tuple[str, list[str]]]
-    """
-    entries = []
-    with open(bibtex_file_path, "r") as f:
-        db = bibtexparser.load(f)
-
-    for entry in db.entries:
-        keywords = []
-        if "keywords" in entry:
-            keywords = entry["keywords"]
-        entries.append((entry["doi"], keywords.split("; ")))
-
-    return entries
