3ae580d3c1d73a2caad00c1c57733ce792837e45
mflan826 <94853197+mflan826@users.noreply.github.com>
2026-02-21 23:34:08 -0500

    further clean up for just having detailed XML cleaning pipeline

--- database.py
+++ database.py
@@ -22,12 +22,6 @@ engine = create_engine(url, poolclass=NullPool, connect_args={'sslmode': os.gete
 
 Base = declarative_base()
 
-class ArticleSummary(Base):
-    __tablename__ = "article_summaries"
-    __table_args__ = {'schema': 'daan_822'}
-    pmid = Column(Text(), primary_key=True,unique=True,nullable=False)
-    sort_date = Column(Date())
-
 class ArticleDetailed(Base):
     __tablename__ = "article_detailed"
     __table_args__ = {'schema': 'daan_822'}
@@ -53,17 +47,6 @@ def create_connection() -> object:
 def close_connection(con):
     con.close()
     return None
-
-def write_data_summary(data,db_connection):
-    for rec in data:
-        if rec["pmid"] is not None:
-            db_connection.add(
-                ArticleSummary(
-                    pmid=rec["pmid"],
-                    sort_date=rec['sortdate']
-                )
-            )
-        db_connection.commit()
 
 def write_data_detailed(data,db_connection):
         for rec in data:
